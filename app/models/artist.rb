c95542ec365dba20dbeb3d23d12bdbcc497adbc8
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-10-22 13:42:22 +0200

    Created index and Sorting filters

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,15 @@
 class Artist < ApplicationRecord
   has_many :songs
+
+  scope :by_name,     -> {order(:name)}
+  scope :created_at,   -> { order(:created_at) }
+
+  def self.order_by_name
+      order(:name)
+  end
+
+  def self.order_by_created_at
+      order(:created_at)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'artists#index'
+
+  resources :artists do
+    collection do
+      get :by_name
+      get :by_created_at
+    end
+  end
 end
